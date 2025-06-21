b7cf0d38d58b6d2777d2683f288cd9b9641ae24c
Revan-Hub69 <topofferte@protonmail.ch>
2025-06-21 16:56:58 +0200

    Update route.ts

--- app/api/files/[fileId]/route.ts
+++ app/api/files/[fileId]/route.ts
@@ -1,11 +1,17 @@
 import { openai } from "@/app/openai";
+import { NextRequest } from "next/server";
 
-// download file by file ID
-export async function GET(_request, { params: { fileId } }) {
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: { fileId: string } }
+) {
+  const { fileId } = params;
+
   const [file, fileContent] = await Promise.all([
     openai.files.retrieve(fileId),
     openai.files.content(fileId),
   ]);
+
   return new Response(fileContent.body, {
     headers: {
       "Content-Disposition": `attachment; filename="${file.filename}"`,
