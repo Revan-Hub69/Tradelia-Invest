32f520c7f1ff48b3454beb2ecf0b5f391d5a4c54
Revan-Hub69 <topofferte@protonmail.ch>
2025-06-21 17:36:41 +0200

    Create Chat.tsx

---
+++ app/chat/Chat.tsx
@@ -0,0 +1,12 @@
+type ChatProps = {
+  userId: string;
+};
+
+export default function Chat({ userId }: ChatProps) {
+  return (
+    <div>
+      {/* Sostituisci con la tua logica */}
+      Chat per User ID: {userId}
+    </div>
+  );
+}
