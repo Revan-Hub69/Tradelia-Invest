76333e1d3a17357be43d6875e2df0187e52015a1
Revan-Hub69 <topofferte@protonmail.ch>
2025-06-21 16:32:06 +0200

    Update openai.ts

--- app/openai.ts
+++ app/openai.ts
@@ -1,3 +1,7 @@
 import OpenAI from "openai";
 
-export const openai = new OpenAI();
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const ASSISTANT_ID = process.env.OPENAI_ASSISTANT_ID as string;
