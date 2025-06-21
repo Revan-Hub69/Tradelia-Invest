3559b6c4de16bbf3135008c2f29103922639f024
Revan-Hub69 <topofferte@protonmail.ch>
2025-06-21 18:05:21 +0200

    Create actions-utils.ts

---
+++ app/utils/actions-utils.ts
@@ -0,0 +1,17 @@
+export const quickActions = [
+  {
+    title: "Consiglio di investimento",
+    description: "Ottieni un consiglio personalizzato",
+    prompt: "Dammi un consiglio di investimento personalizzato.",
+  },
+  {
+    title: "Analisi di mercato",
+    description: "Analizza l'andamento attuale del mercato",
+    prompt: "Fai un'analisi di mercato per oggi.",
+  },
+  {
+    title: "Simulazione di portafoglio",
+    description: "Simula un portafoglio di investimenti",
+    prompt: "Crea una simulazione di portafoglio bilanciato.",
+  },
+];
